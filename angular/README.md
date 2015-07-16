c60bef3904dabe2c17265b85fea5e1a7b54605d3
Hemstreet <jonhemstreet@gmail.com>
2015-07-15 23:13:45 -0600

    basic angular support

---
+++ angular/README.md
@@ -0,0 +1,14 @@
+```
+<p align="center">
+    <img src="path/to/logo.png" alt="Logo"/>
+</p>
+<h3>
+    Web Boiler Plate
+</h3>
+``` 
+Setup
+---
+
+
+
+
---
+++ angular/app/controllers/defaultController.js
@@ -0,0 +1,3 @@
+angular.module('boilerPlate').controller('DemoController', ['$scope', function($scope) {
+    $scope.greeting = 'Hello World From the Controller!';
+}]);
