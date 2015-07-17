32f49f50429576a2e87bb5e9364922cd76689869
Hemstreet <jonhemstreet@gmail.com>
2015-07-16 18:32:28 -0600

    adding express for directives

--- angular/app/directives/demoTag/demoTagDirective.js
+++ angular/app/directives/demoTag/demoTagDirective.js
@@ -1,6 +1,6 @@
 angular.module('boilerPlate').directive('demoTag', function() {
     return {
         restrict: 'E',
-        templateUrl: '~/Development/Sandbox/Javascript-Boilerplates/angular/app/directives/demoTag/demoTag.html'
+        templateUrl: './directives/demoTag/demoTag.html'
     }
 });
